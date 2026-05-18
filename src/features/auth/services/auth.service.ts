f9195949f5af588dc7a2dc5b85d22544cc5eac27
Growthen <arielandrerevilla@gmail.com>
2026-05-17 20:41:33 -0500

    feat: Se crearon auth.service y token.service para obtener, guardar y eliminar el token

---
+++ src/features/auth/services/auth.service.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import type { LoginRequest, AuthResponse } from "../types/auth.types";
+import { getToken } from "./token.service";
+
+const API_BASE_URL = "http://10.0.2.2:8080";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const loginService = async (data: LoginRequest): Promise<AuthResponse> => {
+  const response = await api.post<AuthResponse>("/api/auth/login", data);
+  return response.data;
+};
+
+export default api;
