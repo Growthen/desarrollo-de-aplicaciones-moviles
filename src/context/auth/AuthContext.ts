cd4f7b62264468dd72dba3662de1f66b01cc425b
Growthen <arielandrerevilla@gmail.com>
2026-05-21 20:12:19 -0500

    feat: "Se agrego Dev Mode"

--- src/context/auth/AuthContext.ts
+++ src/context/auth/AuthContext.ts
@@ -19,6 +19,7 @@ export type AuthContextType = {
     password: string,
   ) => Promise<void>;
   logout: () => void;
+  devLogin?: (role: AuthRole) => Promise<void>;
   isLoading: boolean;
   error: string | null;
 };
